719a660adedc7145d21ca0e1a1a7ab7043b37ff9
Sergey Sviridenko <Sergey.Sviridenko@gmail.com>
2013-05-04 14:08:57 +0300

    I write code to parse sentiment file into dictionary plus tweets

--- Homework1/Problem2/tweet_sentiment.py
+++ Homework1/Problem2/tweet_sentiment.py
@@ -1,4 +1,9 @@
+''' Problem 2: Derive the sentiment of each tweet'''
+
+import json
 import sys
+import string
+import re
 
 def hw():
     print 'Hello, world!'
@@ -13,5 +18,25 @@ def main():
     lines(sent_file)
     lines(tweet_file)
 
-if __name__ == '__main__':
-    main()
+##if __name__ == '__main__':
+##    main()
+
+sentiment_file = open('AFINN-111.txt','r')
+data = open("outputtop20.txt",'r')
+
+
+sentiment_dict = dict()
+for line in sentiment_file.readlines():
+    line_sp =  line[:-1].split("\t")
+    try:
+        sentiment_dict[line_sp[0]] = int(line_sp[1])
+    except ValueError:
+        sentiment_dict[line_sp[0]] = 0
+        
+for line in data.readlines():
+    tweet = json.loads(line[:-1],encoding='utf-8')
+    try:
+        tweet_text = tweet['text']
+    except KeyError:
+        tweet_text = ""    
+    text_parsed = re.sub('[^A-Za-z0-9 ]+','',tweet_text).lower().split(" ")
