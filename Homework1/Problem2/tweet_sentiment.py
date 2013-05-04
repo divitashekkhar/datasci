735b05005983964d27b2f3655ddff4d656879f91
Sergey Sviridenko <Sergey.Sviridenko@gmail.com>
2013-05-04 14:18:18 +0300

    I enclose code for sentiment dict into a function

--- Homework1/Problem2/tweet_sentiment.py
+++ Homework1/Problem2/tweet_sentiment.py
@@ -5,12 +5,26 @@ import sys
 import string
 import re
 
+sentiment_file_path ='AFINN-111.txt'
+
 def hw():
     print 'Hello, world!'
 
 def lines(fp):
     print str(len(fp.readlines()))
 
+def get_sentiment_dict(sentiment_file_path)
+    sentiment_file = open(sentiment_file_path,'r')
+    sentiment_dict = dict()
+    for line in sentiment_file.readlines():
+        line_sp =  line[:-1].split("\t")
+        try:
+            sentiment_dict[line_sp[0]] = int(line_sp[1])
+        except ValueError:
+            sentiment_dict[line_sp[0]] = 0
+    return sentiment_dict
+
+    
 def main():
     sent_file = open(sys.argv[1])
     tweet_file = open(sys.argv[2])
@@ -21,22 +35,10 @@ def main():
 ##if __name__ == '__main__':
 ##    main()
 
-sentiment_file = open('AFINN-111.txt','r')
 data = open("outputtop20.txt",'r')
 
+sentiment_dict = get_sentiment_dict(sentiment_file_path)
 
-sentiment_dict = dict()
-for line in sentiment_file.readlines():
-    line_sp =  line[:-1].split("\t")
-    try:
-        sentiment_dict[line_sp[0]] = int(line_sp[1])
-    except ValueError:
-        sentiment_dict[line_sp[0]] = 0
-        
-for line in data.readlines():
-    tweet = json.loads(line[:-1],encoding='utf-8')
-    try:
-        tweet_text = tweet['text']
-    except KeyError:
-        tweet_text = ""    
-    text_parsed = re.sub('[^A-Za-z0-9 ]+','',tweet_text).lower().split(" ")
+tot_sentiment = 0
+for word in text_parsed:
+    tot_sentiment += sentiment_dict[word]
