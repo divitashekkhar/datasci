8d60ce18a8c3abc8254fcae614705a46ea8bdce1
Sergey Sviridenko <Sergey.Sviridenko@gmail.com>
2013-05-04 14:21:06 +0300

    Last deleted some by mistake

--- Homework1/Problem2/tweet_sentiment.py
+++ Homework1/Problem2/tweet_sentiment.py
@@ -39,6 +39,15 @@ data = open("outputtop20.txt",'r')
 
 sentiment_dict = get_sentiment_dict(sentiment_file_path)
 
+for line in data.readlines():
+    tweet = json.loads(line[:-1],encoding='utf-8')
+    try:
+        tweet_text = tweet['text']
+    except KeyError:
+        tweet_text = ""    
+    text_parsed = re.sub('[^A-Za-z0-9 ]+','',tweet_text).lower().split(" ")
+
 tot_sentiment = 0
 for word in text_parsed:
     tot_sentiment += sentiment_dict[word]
+
