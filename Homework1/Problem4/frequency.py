ef9d21f3e63d7f1e3f11444d7391492459320eb9
Sergey Sviridenko <Sergey.Sviridenko@gmail.com>
2013-05-05 11:21:03 +0300

    Got working program, although it counts letters not words frequency, LOL

--- Homework1/Problem4/frequency.py
+++ Homework1/Problem4/frequency.py
@@ -1,35 +1,10 @@
 '''Problem 4: Compute Term Frequency
 Write a Python script, frequency.py, to compute the term frequency histogram of the livestream data you harvested from Problem 1.'''
 
-def get_tweets_scored(tweet_file_path, sentiment_dict):
-    tweets_scored = []
-    data = open(tweet_file_path,'r')
-    for line in data.readlines():
-        text_parsed = parse_tweet_text(line)
-        for word in text_parsed:
-            score = get_sentiment_score(text_parsed,sentiment_dict)   
-        tweets_scored.append( (text_parsed, score) )
-    return tweets_scored
-
-def get_terms_used(tweets):
-    terms_all = []
-    for tweet in tweets:
-        for word in tweet[0]:
-            terms_all.append(word)
-    terms_used = set(terms_all)
-    return terms_used
-
-def score_terms(terms,tweets_scored):
-    terms_observed = { item: [0,0] for item in terms} 
-    for tweet in tweets_scored:
-        for word in tweet[0]:
-            try:
-                terms_observed[word][0] += tweet[1]
-                terms_observed[word][1] += 1
-            except KeyError:
-                pass
-    terms_scored = { key:  terms_observed[key][0]/terms_observed[key][1] for key in terms_observed }
-    return terms_scored
+import sys
+import re
+import string
+import json
 
 def parse_tweet_text(line):
     tweet = json.loads(line[:-1],encoding='utf-8')
@@ -39,19 +14,51 @@ def parse_tweet_text(line):
         tweet_text = ""    
     text_parsed = re.sub('[^A-Za-z0-9 ]+','',tweet_text).lower().split(" ")
     return text_parsed
+
+def get_tweets_parsed(tweet_file_path):
+    tweets_parsed = []
+    data = open(tweet_file_path,'r')
+    for line in data.readlines():
+        text_parsed = parse_tweet_text(line)
+        tweets_parsed.append( text_parsed )
+    return tweets_parsed
+
+def get_terms_used(tweets):
+    terms_all = []
+    for tweet in tweets:
+        for word in tweet[0]:
+            terms_all.append(word)
+    terms_used = set(terms_all)
+    return terms_used
+
+def count_terms(terms,tweets_parsed):
+    terms_observed = { item: 0.0 for item in terms}
+    tot_count = 0.0
+    for tweet in tweets_parsed:
+        for word in tweet:
+            try:
+                terms_observed[word] += 1
+                tot_count += 1
+            except KeyError:
+                pass
+    terms_frequency = { key:  terms_observed[key]/tot_count for key in terms_observed }
+    return terms_frequency
     
 def main():
-    sent_file_path = (sys.argv[1])
-    tweet_file_path = (sys.argv[2])
 
-    sentiment_dict = get_sentiment_dict(sent_file_path)
-    tweets_scored = get_tweets_scored(tweet_file_path, sentiment_dict)
+    tweet_file_path = (sys.argv[1])
 
-    terms_all = get_terms_used(tweets_scored)
-    terms_new = exclude_old_terms(terms_all,sentiment_dict)
-    res = score_terms(terms_new, tweets_scored)
+    tweets_parsed = get_tweets_parsed(tweet_file_path)
 
-    del res['']
+    terms_all = get_terms_used(tweets_parsed)
+
+    res = count_terms(terms_all, tweets_parsed)
+
+    try:
+        del res['']
+    except:
+        pass
+    
     for key in res:
         print key, res[key]
 
