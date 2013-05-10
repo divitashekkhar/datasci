67cf8286419777c0d488c103f2a94e5fd4760027
Sergey Sviridenko <Sergey.Sviridenko@gmail.com>
2013-05-10 20:49:33 +0300

    Parsed hashtags out of tweets, however results are negative. I need to refactor this legacy code

--- Homework1/Problem6/top_ten.py
+++ Homework1/Problem6/top_ten.py
@@ -8,42 +8,48 @@ import json
 
 def parse_tweet_text(line):
     tweet = json.loads(line[:-1],encoding='utf-8')
+    res = []
     try:
-        tweet_text = tweet['text']
+        tags = tweet['entities']['hashtags']
+        if len(tags) == 0:
+            return res
+        else:
+            for tag in tags:
+                res.append( tag['text'] )
     except KeyError:
-        tweet_text = ""    
-    text_parsed = re.sub('[^A-Za-z0-9 ]+','',tweet_text).split(" ")
-    return [word for word in text_parsed if len(word)>2 ]
+        return res
 
 def get_tweets_parsed(tweet_file_path):
     tweets_parsed = []
     data = open(tweet_file_path,'r')
     for line in data.readlines():
-        text_parsed = parse_tweet_text(line)
-        tweets_parsed.extend( text_parsed )
+        tags = parse_tweet_text(line)
+        try:
+            tweets_parsed.extend( tags )
+        except TypeError:
+            pass
     return tweets_parsed
 
-def get_terms_used(tweets):
-    terms_used = set(tweets)
-    return terms_used
+def get_tags_used(tweets):
+    tags_used = set(tweets)
+    return tags_used
 
-def count_terms(terms,tweets_parsed):
-    terms_observed = { item: 0.0 for item in terms}
+def count_tags(tags,tweets_parsed):
+    tags_observed = { item: 0.0 for item in tags}
     tot_count = 0.0
     
     try:
-        del terms_observed['']
+        del tags_observed['']
     except:
         pass
 
     for word in tweets_parsed:
         try:
-            terms_observed[word] += 1
-            tot_count += 1
+            tags_observed[word] += 1
         except KeyError:
             pass
-    terms_frequency = { key:  terms_observed[key]/tot_count for key in terms_observed }
-    return terms_frequency
+    tags_count = { key:  tags_observed[key] for key in tags_observed }
+    return tags_count
     
 def main():
 
@@ -51,9 +57,9 @@ def main():
 
     tweets_parsed = get_tweets_parsed(tweet_file_path)
 
-    terms_all = get_terms_used(tweets_parsed)
+    tags_all = get_tags_used(tweets_parsed)
 
-    res = count_terms(terms_all, tweets_parsed)
+    res = count_tags(tags_all, tweets_parsed)
 
     for key in res:
         print key, res[key]
