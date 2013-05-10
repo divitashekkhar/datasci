a0b4acf82b333c914bc27627691dd9b181dd0f81
Sergey Sviridenko <Sergey.Sviridenko@gmail.com>
2013-05-10 20:12:19 +0300

    Initial commit for 6th task, would heavily rely on 4th solution

---
+++ Homework1/Problem6/README.txt
@@ -0,0 +1,10 @@
+Problem 6: Top ten hash tags
+
+According to the folowing task description:
+ 
+Write a Python script, top_ten.py, that computes the ten most frequently occurring hash tags from the data you gathered in Problem 1.
+top_ten.py should take a file of tweets as an input and be usable in the following way:
+          $ python top_ten.py <tweet_file>
+Assume the tweet file contains data formatted the same way as the livestream data.
+Your script should print to stdout each hashtag-count pair, one per line, in the following format:     
+          <hashtag:string> <count:float>
---
+++ Homework1/Problem6/frequency.py
@@ -0,0 +1,62 @@
+'''Problem 4: Compute Term Frequency
+Write a Python script, frequency.py, to compute the term frequency histogram of the livestream data you harvested from Problem 1.'''
+
+import sys
+import re
+import string
+import json
+
+def parse_tweet_text(line):
+    tweet = json.loads(line[:-1],encoding='utf-8')
+    try:
+        tweet_text = tweet['text']
+    except KeyError:
+        tweet_text = ""    
+    text_parsed = re.sub('[^A-Za-z0-9 ]+','',tweet_text).split(" ")
+    return [word for word in text_parsed if len(word)>2 ]
+
+def get_tweets_parsed(tweet_file_path):
+    tweets_parsed = []
+    data = open(tweet_file_path,'r')
+    for line in data.readlines():
+        text_parsed = parse_tweet_text(line)
+        tweets_parsed.extend( text_parsed )
+    return tweets_parsed
+
+def get_terms_used(tweets):
+    terms_used = set(tweets)
+    return terms_used
+
+def count_terms(terms,tweets_parsed):
+    terms_observed = { item: 0.0 for item in terms}
+    tot_count = 0.0
+    
+    try:
+        del terms_observed['']
+    except:
+        pass
+
+    for word in tweets_parsed:
+        try:
+            terms_observed[word] += 1
+            tot_count += 1
+        except KeyError:
+            pass
+    terms_frequency = { key:  terms_observed[key]/tot_count for key in terms_observed }
+    return terms_frequency
+    
+def main():
+
+    tweet_file_path = (sys.argv[1])
+
+    tweets_parsed = get_tweets_parsed(tweet_file_path)
+
+    terms_all = get_terms_used(tweets_parsed)
+
+    res = count_terms(terms_all, tweets_parsed)
+
+    for key in res:
+        print key, res[key]
+
+if __name__ == '__main__':
+    main()
