1589ba44853290a4f80a87acc6e0c7cca69384fd
Sergey Sviridenko <Sergey.Sviridenko@gmail.com>
2013-05-10 17:52:00 +0300

    Starting 5th task, in this one i would heavily use solution for 2nd task, therefore most file is copied from there

---
+++ Homework1/Problem5/README.txt
@@ -0,0 +1,11 @@
+Which State is happiest?
+
+Task description:
+Write a Python script, happiest_state.py, that returns the name of the happiest state as a string.
+happiest_state.py should take a file of tweets as an input and be usable in the following way:
+          $ python happiest_state.py <sentiment_file> <tweet_file>
+The file AFINN-111.txt contains a list of pre-computed sentiment score.
+Assume the tweet file contains data formatted the same way as the livestream data.
+
+ 
+As it was recommended that I would build on my solution to Problem 2.
---
+++ Homework1/Problem5/tweet_sentiment.py
@@ -0,0 +1,49 @@
+''' Problem 2: Derive the sentiment of each tweet'''
+
+import json
+import sys
+import string
+import re
+
+def get_sentiment_dict(sentiment_file_path):
+    sentiment_file = open(sentiment_file_path,'r')
+    sentiment_dict = dict()
+    for line in sentiment_file.readlines():
+        line_sp =  line[:-1].split("\t")
+        try:
+            sentiment_dict[line_sp[0]] = int(line_sp[1])
+        except ValueError:
+            sentiment_dict[line_sp[0]] = 0
+    return sentiment_dict
+
+def get_sentiment_score(text_parsed,sentiment_dict):
+    tot_sentiment = 0.0
+    for word in text_parsed:
+        try:
+            tot_sentiment += sentiment_dict[word]
+        except KeyError:
+            tot_sentiment += 0.0
+    return tot_sentiment
+
+def parse_tweet_text(line):
+    tweet = json.loads(line[:-1],encoding='utf-8')
+    try:
+        tweet_text = tweet['text']
+    except KeyError:
+        tweet_text = ""    
+    text_parsed = re.sub('[^A-Za-z0-9 ]+','',tweet_text).lower().split(" ")
+    return text_parsed
+    
+def main():
+    sentiment_file_path = (sys.argv[1])
+    tweet_file_path     = (sys.argv[2])
+
+    data = open(tweet_file_path,'r')
+    sentiment_dict = get_sentiment_dict(sentiment_file_path)
+
+    for line in data.readlines():
+        text_parsed = parse_tweet_text(line)
+        print get_sentiment_score(text_parsed,sentiment_dict)
+
+if __name__ == '__main__':
+    main()
