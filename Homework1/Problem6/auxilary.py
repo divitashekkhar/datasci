6f758772fb734639c9824ce9108e583ab7e639a1
Sergey Sviridenko <Sergey.Sviridenko@gmail.com>
2013-05-11 11:48:58 +0300

    using for exploration, just import main file and run/debug commands you want

--- Homework1/Problem6/auxilary.py
+++ Homework1/Problem6/auxilary.py
@@ -2,17 +2,8 @@ import json
 import sys
 import string
 import re
+import top_ten
+
 
 tweet_file_path = 'output.txt'
 
-data = open(tweet_file_path,'r') 
-for line in data.readlines():
-    tweet = json.loads(line[:-1],encoding='utf-8')
-    try:
-        l = tweet['entities']['hashtags']
-        if len(l) > 0:
-            break
-        else:
-            pass
-    except KeyError:
-        pass
