54f47b01d65c9883e706ecdd5458ccf9a0d25858
Sergey Sviridenko <Sergey.Sviridenko@gmail.com>
2013-05-19 15:05:17 +0300

    Problem 6th is solved

--- Homework3/Problem6/multiply.py
+++ Homework3/Problem6/multiply.py
@@ -9,6 +9,12 @@ import sys
 mr = MapReduce.MapReduce()
 
 
+# We assume that matrix dimensions is known beforehand
+# A  is MxK matrix, whereas B is KxN one
+M = 5
+K = 5
+N = 5
+
 def mapper(record):
     '''
     The input to the map function will be matrix row records formatted as lists.
@@ -17,14 +23,42 @@ def mapper(record):
         'a' indicates that the record is from matrix A
         'b' indicates that the record is from matrix B
     '''
-    mr.emit_intermediate( )
+    matrix = record[0]
+    row = record[1]
+    col = record[2]
+    value = record[3]
+    
+    if matrix == 'a':
+        for n in range(N):
+            destination_cell = (row,n)
+            matrix_position = 'L'
+            serial_number = col
+            mr.emit_intermediate( destination_cell, (matrix_position ,serial_number, value) )
+
+    if matrix == 'b':
+        for n in range(N):
+            destination_cell = (n,col)
+            matrix_position = 'R'
+            serial_number = row
+            mr.emit_intermediate( destination_cell, (matrix_position ,serial_number, value) )
 
 def reducer(key, list_of_values):
     '''
     The output from the reduce function will also be matrix row records formatted as tuples.
     Each tuple will have the format (i, j, value) where each element is an integer.
-    '''        
-    mr.emit()
+    '''
+    
+    left_matrix_value = [ (item[1],item[2]) for item in list_of_values if item[0] == 'L' ]
+    right_matrix_value = [ (item[1],item[2]) for item in list_of_values if item[0] == 'R' ]
+
+    result = 0
+
+    for item_L in left_matrix_value:
+        for item_R in right_matrix_value:
+            if item_L[0] == item_R[0] :
+                result += item_L[1] * item_R[1]
+
+    mr.emit( ( key[0], key[1], result ) )
 
 inputdata = open(sys.argv[1])
 mr.execute(inputdata, mapper, reducer)
